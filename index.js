dc5c599fd5f20f5117ad269690b8aabcd509363b
Elizaveta <65027713+ElizavetaKubik@users.noreply.github.com>
2022-03-21 10:54:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const hbs = require("express-handlebars").create({ extname: ".hbs" });
+const bodyParser = require("body-parser");
+
+const NumbersRouter = require("./routes/numbers.route");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+app.use(express.static(__dirname + "/public"));
+
+app.use("/", NumbersRouter);
+
+app.listen(3000, () => {
+  console.log("Started on 3000 port.");
+});
